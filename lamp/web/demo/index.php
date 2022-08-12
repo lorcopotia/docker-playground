893ebb39ffdf7a20ceb06dbeccab428d7d548782
Duanel Garrido Milan <lorcopotia@gmail.com>
2022-08-12 10:54:45 +0200

    Update index.php

--- lamp/web/demo/index.php
+++ lamp/web/demo/index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
+<?php
+echo phpinfo();
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
-
-</body>
-</html> 
+?>
